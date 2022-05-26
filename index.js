3cdb929b99af8f18039af5848ec18d28628e1d8e
Naomi Cole <naomisarahcole@gmail.com>
2022-05-26 13:03:40 +1000

    10 passing tests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var a = 2;
 var b = 50;
+var n =35;
 
 function add(a,b) {
     return a+= b;
@@ -15,4 +16,14 @@ function multiply(a,b) {
 
 function divide(a,b) {
     return a/= b;
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
 }
