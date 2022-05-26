13270ebd949497a1123a3067b1cc8c33fc1310e3
Naomi Cole <naomisarahcole@gmail.com>
2022-05-26 13:01:03 +1000

    8 Passing tests

---
+++ index.js
@@ -0,0 +1,18 @@
+var a = 2;
+var b = 50;
+
+function add(a,b) {
+    return a+= b;
+}
+
+function subtract(a,b) {
+    return a-= b;
+}
+
+function multiply(a,b) {
+    return a*= b;
+}
+
+function divide(a,b) {
+    return a/= b;
+}
